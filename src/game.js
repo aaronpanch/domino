93bbcfc7ded8a9ef23574fcd3a47e8bd3891c341
Aaron Panchal <aaron.panchal@appfolio.com>
2017-02-21 23:47:15 -0800

    Add shuffler and newGame functions

--- src/game.js
+++ src/game.js
@@ -1,8 +1,34 @@
+const shuffle = require('./shuffle');
+
+const DEFAULT_HAND_SIZE = 7;
+
 /*
   A game is an object:
   {
-    bones: Array<Domino>,
-    players: Array<Player>,
-    board:
+    bones: Array<Domino>
   }
 */
+
+function newGame(tileset, numPlayers, handSize = DEFAULT_HAND_SIZE) {
+  if (tileset.length / numPlayers < handSize) {
+    throw 'Too many players!';
+  }
+
+  const tiles = shuffle(tileset);
+
+  const players = Array(numPlayers).fill(undefined).map((a, i) => {
+    return {
+      tiles: tiles.slice(handSize * i, handSize * i + handSize),
+      score: 0
+    };
+  });
+
+  return {
+    bones: tiles.slice(numPlayers * handSize),
+    players
+  };
+}
+
+module.exports = {
+  newGame
+}
---
+++ test/game.spec.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect
+
+const game = require('../src/game');
+const { generateTiles } = require('../src/domino');
+
+describe('game', () => {
+  describe('newGame', () => {
+    const { newGame } = game;
+
+    it('should validate number of players for default hand', () => {
+      expect(() => newGame(Array(13), 2)).to.throw('Too many players!');
+    });
+
+    it('should validate number of players for custom hand', () => {
+      expect(() => newGame(Array(5), 2, 3)).to.throw('Too many players!');
+    });
+
+    it('should distribute tiles to players', () => {
+      const tileset = generateTiles();
+      const gameState = newGame(tileset, 3);
+
+      expect(gameState.bones).to.have.lengthOf(7);
+      expect(gameState.players).to.have.lengthOf(3);
+
+      gameState.players.forEach(player => {
+        expect(player.tiles).to.have.lengthOf(7);
+        expect(player.score).to.equal(0);
+      });
+    });
+  });
+});
