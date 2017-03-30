be411409e8c053b39f1f475e2d65cc9480ea9491
Aaron Panchal <aaron.panchal@appfolio.com>
2017-03-29 21:33:30 -0700

    Revise board structure and add leaves function

--- src/board.js
+++ src/board.js
@@ -1,24 +1,50 @@
 /*
   A board is an object representing an arrangement of Domino tiles
-  It mimics a two dimentional array (referenced X then Y).
+  It consists of two parts:
+    1) tiles: mimics a two dimentional array (referenced X then Y)
+    2) spinner: a coordinate reference to the spinner
 
     {
-      [Number (X)]: {
-        [Number (Y)]: Domino
+      tiles: {
+        [Number (X)]: {
+          [Number (Y)]: Domino
+        }
       }
+      spinner: [Number, Number] | null
     }
 
     e.g.
     {
-      -1: { 0: [2,6] },
-       0: { -1: [3,6], 0: [6,6], 1: [6,5] },
-       1: { 0: [6,4] }
+      tiles: {{
+        -1: { 0: [2,6] },
+         0: { -1: [3,6], 0: [6,6], 1: [6,5] },
+         1: { 0: [6,4] }
+      },
+      spinner: [0,0]
     }
 */
 
-const get = (board, x, y) =>
-  board[x] && board[x][y]
+const get = ({ tiles }, [x, y]) =>
+  tiles[x] && tiles[x][y]
+
+const leaves = ({ tiles, spinner }) => {
+  const xRange = Object.keys(tiles);
+
+  let leaves = [
+    [Math.min(...xRange), 0],
+    [Math.max(...xRange), 0]
+  ];
+
+  if (spinner) {
+    const yRange = Object.keys(tiles[spinner[0]]);
+    leaves.push([spinner[0], Math.min(...yRange)]);
+    leaves.push([spinner[0], Math.max(...yRange)]);
+  }
+
+  return leaves;
+}
 
 module.exports = {
-  get
+  get,
+  leaves
 }
--- test/board.spec.js
+++ test/board.spec.js
@@ -4,26 +4,41 @@ const Board = require('../src/board');
 
 describe('Board', () => {
   const testBoard = {
-    '-1': { 0: [2,6] },
-       0: {
-        '-1': [3,6],
-           0: [6,6],
-           1: [6,5]
-       },
-       1: { 0: [6,4] }
+    tiles: {
+      '-1': { 0: [2,6] },
+         0: {
+          '-1': [3,6],
+             0: [6,6],
+             1: [6,5]
+         },
+         1: { 0: [6,4] }
+    },
+    spinner: [0,0]
   }
 
   describe('get', () => {
     it('should get the domino at the given coordinates', () => {
-      expect(Board.get(testBoard, 0, 0)).to.deep.equal([6,6]);
-      expect(Board.get(testBoard, -1, 0)).to.deep.equal([2,6]);
-      expect(Board.get(testBoard, 0, 1)).to.deep.equal([6,5]);
+      expect(Board.get(testBoard, [0,0])).to.deep.equal([6,6]);
+      expect(Board.get(testBoard, [-1,0])).to.deep.equal([2,6]);
+      expect(Board.get(testBoard, [0,1])).to.deep.equal([6,5]);
     });
 
     it('should return undefined for when there is no domino', () => {
-      expect(Board.get(testBoard, -1, 1)).to.be.undefined;
-      expect(Board.get(testBoard, 2, 0)).to.be.undefined;
-      expect(Board.get(testBoard, 0, 2)).to.be.undefined;
+      expect(Board.get(testBoard, [-1,1])).to.be.undefined;
+      expect(Board.get(testBoard, [2,0])).to.be.undefined;
+      expect(Board.get(testBoard, [0,2])).to.be.undefined;
+    });
+  });
+
+  describe('leaves', () => {
+    it('should return the coordinates of the board leaves', () => {
+      const leaves = Board.leaves(testBoard);
+      expect(leaves).to.deep.equal([
+        [-1,0],
+        [1,0],
+        [0,-1],
+        [0,1]
+      ]);
     });
   });
 });
